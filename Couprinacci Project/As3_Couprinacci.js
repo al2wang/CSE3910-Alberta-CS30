230bf48374bc4e92a9ff3b207472540f0e688fce
Guangyuan Wang <104330029+al2wang@users.noreply.github.com>
2022-06-19 18:59:20 -0600

    Add files via upload

---
+++ Couprinacci Project/As3_Couprinacci.js
@@ -0,0 +1,28 @@
+  let recursiveruns = 0;
+function coupSequence(num) {
+  recursiveruns++;
+  if (num <= 3){
+    if (num == 1)
+      return 3;
+    if (num == 2)
+      return 5;
+    if (num == 3)
+      return 8;
+  }
+    else {
+      return coupSequence(num - 3) + coupSequence(num - 2) + coupSequence(num - 1);
+    }
+}
+
+// take nth term input from the user
+const nTerm = prompt('Generate which term number? ');
+
+if(nTerm <=0) {
+    console.log('Enter a positive integer.');
+}
+else {
+  console.log(coupSequence(nTerm));
+    }
+
+console.log('Term #'+nTerm+' is '+coupSequence(nTerm));
+console.log("Program complete. The recursive function ran " + recursiveruns + " times.")
